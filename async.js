e628a839e76e253c7096bb69f3572ca1e4d5ad2b
sara-trueigtech <sakhan@trueigtech.com>
2026-01-14 18:06:54 +0530

    feat: promises

--- async.js
+++ async.js
@@ -12,11 +12,51 @@
 
 // console.log("End");
 
-const btn = document.querySelector("#btn");
-const text = document.querySelector("#text");
+// const btn = document.querySelector("#btn");
+// const text = document.querySelector("#text");
 
-btn.addEventListener("click", () => {
-  setTimeout(() => {
-    text.textContent = "This appears after 2 seconds";
-  }, 2000);
+// btn.addEventListener("click", () => {
+//   setTimeout(() => {
+//     text.textContent = "This appears after 2 seconds";
+//   }, 2000);
+// });
+
+// creating a promise
+const myPromise = new Promise((resolve, reject) => {
+  let success = true;
+
+  if (success) {
+    resolve("Task completed");
+  } else {
+    reject("Task failed");
+  }
+});
+
+// Consuming a Promise
+myPromise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const loadData = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("data loaded success");
+    }, 2000);
+  });
+};
+
+document.querySelector("#load").addEventListener("click", () => {
+  document.querySelector("#status").textContent = "Loading...";
+
+  loadData()
+    .then((data) => {
+      document.querySelector("#status").textContent = data;
+    })
+    .catch((err) => {
+      document.querySelector("#status").textContent = err;
+    });
 });
