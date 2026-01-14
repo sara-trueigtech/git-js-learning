1ddeae51664a84721e459b3ee6cbfdf196633800
sara-trueigtech <sakhan@trueigtech.com>
2026-01-14 12:39:12 +0530

    feat : add object and arrayexample

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ console.log("hello javascript");
 let user = "sara";
 const role  = "Developer";
 console.log(user, role);
+
+
+const userDetails = {
+    name : "sneha",
+    skills : ["js", "git", "react"]
+};
+
+userDetails.skills.forEach(s => console.log(s));
