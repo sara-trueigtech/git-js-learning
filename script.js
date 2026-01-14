e983958eb8ad3229cf9c6539559327ce28c00c43
sara-trueigtech <sakhan@trueigtech.com>
2026-01-14 14:59:06 +0530

    feat: events and event listener

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ document.querySelector('#title');
 document.querySelector('.text');
 document.querySelector('button');
 
+// returns a array like list of all the matched elements
 document.querySelectorAll("p").forEach(p => 
     {console.log(p);
 });
+
+// events and event listener
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => 
+{
+    alert("button clicked!");
+})
+
+const input = document.querySelector(".input");
+
+input.addEventListener("input", (e) => {
+    console.log(e.target.value);
+})
