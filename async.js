c2135d04dc37433cc391b31aa24bb3f923a8c467
sara-trueigtech <sakhan@trueigtech.com>
2026-01-14 17:48:01 +0530

    feat: setTimeout

--- async.js
+++ async.js
@@ -1,13 +1,22 @@
 // syncronous code
-console.log("Start");
-console.log("Middle");
-console.log("End");
+// console.log("Start");
+// console.log("Middle");
+// console.log("End");
 
-// ASYNCHRONOUS
-console.log("Start");
+// // ASYNCHRONOUS
+// console.log("Start");
 
-setTimeout(() => {
-  console.log("Middle");
-}, 2000);
+// setTimeout(() => {
+//   console.log("Middle");
+// }, 2000);
 
-console.log("End");
+// console.log("End");
+
+const btn = document.querySelector("#btn");
+const text = document.querySelector("#text");
+
+btn.addEventListener("click", () => {
+  setTimeout(() => {
+    text.textContent = "This appears after 2 seconds";
+  }, 2000);
+});
