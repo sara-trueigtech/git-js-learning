1796a08712f0389be2dfb3ce9f0fabbc356c509a
sara-trueigtech <sakhan@trueigtech.com>
2026-01-14 15:29:54 +0530

    feat: event propogation

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 // return first matching element
-document.querySelector('#title');
-document.querySelector('.text');
-document.querySelector('button');
+// document.querySelector("#title");
+// document.querySelector(".text");
+// document.querySelector("button");
 
-// returns a array like list of all the matched elements
-document.querySelectorAll("p").forEach(p => 
-    {console.log(p);
+// // returns a array like list of all the matched elements
+// document.querySelectorAll("p").forEach((p) => {
+//   console.log(p);
+// });
+
+// // events and event listener
+// const btn = document.querySelector("#btn");
+
+// btn.addEventListener("click", () => {
+//   alert("button clicked!");
+// });
+
+// const input = document.querySelector(".input");
+
+// input.addEventListener("input", (e) => {
+//   console.log(e.target.value);
+// });
+
+// event propogation
+// Event Flow Order
+// Capturing phase (top → target)
+// Target phase
+// Bubbling phase (target → top)
+
+document.querySelector("#parent").addEventListener("click", () => {
+  console.log("parent clicked");
 });
 
-// events and event listener
-const btn = document.querySelector("#btn");
-
-btn.addEventListener("click", () => 
+document.querySelector("#child").addEventListener("click", (e) => 
 {
-    alert("button clicked!");
-})
-
-const input = document.querySelector(".input");
-
-input.addEventListener("input", (e) => {
-    console.log(e.target.value);
-})
+    console.log("child clicked");
+    e.stopPropagation();
+});
