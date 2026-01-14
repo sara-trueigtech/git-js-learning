317a9f6f0818da3780bac7e6035501d06804a599
sara-trueigtech <sakhan@trueigtech.com>
2026-01-14 17:28:56 +0530

    feat: arrow functions

--- function.js
+++ function.js
@@ -5,18 +5,51 @@
 
 // greet();
 
-const btn = document.querySelector("#btn");
-const greet = () => {
-    console.log("hello from func expression");
-}
+// const btn = document.querySelector("#btn");
+// const greet = () => {
+//     console.log("hello from func expression");
+// }
 
-btn.addEventListener("click", greet);
+// btn.addEventListener("click", greet);
 
-const add = (a, b) => {
-    return a + b;
-}
+// const add = (a, b) => {
+//     return a + b;
+// }
 
-// implicit run arrow function
-const addtwo = (a,b) => a + b;
+// // implicit run arrow function
+// const addtwo = (a,b) => a + b;
 
-console.log(addtwo(8,5));
+// console.log(addtwo(8,5));
+
+// this behavior in Normal Function
+// const obj = {
+//   name: "Sara",
+//   greet: function () {
+//     console.log(this.name);
+//   }
+// };
+
+// obj.greet(); 
+
+// this behavior in arrow Function
+// Arrow functions DO NOT have their own this They take this from parent scope
+// const obj = {
+//   name: "Sara",
+//   greet: () => {
+//     console.log(this.name);
+//   }
+// };
+
+// obj.greet(); 
+
+// Event Listener Difference Arrow (BAD here)
+// const btn = document.querySelector("#btn");
+// btn.addEventListener("click", () => {
+//   console.log(this); // window object
+// });
+
+// Normal (GOOD)
+// btn.addEventListener("click", function () {
+//   console.log(this); // button
+// });
+
