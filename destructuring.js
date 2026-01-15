d7dad2d51d80c62d293c20dcd5564ccca67fac8d
sara-trueigtech <sakhan@trueigtech.com>
2026-01-15 18:33:15 +0530

    feat : spread and rest operators

--- destructuring.js
+++ destructuring.js
@@ -17,4 +17,33 @@ const {
 } = student; //Nested Object Destructuring
 
 document.getElementById("output").textContent =
-  name + " - " + branch;
+    userName + " - " + branch;
+
+
+  const colors = ["Red", "Green", "Blue"];
+
+const [first, second] = colors;
+
+console.log(first);  // Red
+console.log(second); // Green
+
+const [,, third] = colors;
+
+console.log(third); // Blue
+
+const [a, b = 10] = [5]; //Default Values
+
+// Swapping Values
+let x = 5;
+let y = 10;
+
+[x, y] = [y, x];
+console.log(x, y);
+
+const [f, ...others] = [1, 2, 3, 4];
+console.log(f, others);
+
+const arr = [15];
+
+console.log(arr[0]?.name);
+
