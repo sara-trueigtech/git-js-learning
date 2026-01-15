20c5571b9994ea0bd6c42880469eaec7ce43c145
sara-trueigtech <sakhan@trueigtech.com>
2026-01-15 16:19:14 +0530

    feat : array method map()

--- arrays.js
+++ arrays.js
@@ -5,10 +5,18 @@
 // console.log(numbers[0]); 
 // console.log(numbers[2]); 
 
-const numbers = [1, 2, 3];
+// const numbers = [1, 2, 3];
 
-for (let i = 0; i < numbers.length; i++) {
-  console.log(numbers[i]);
-}
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
 
-numbers.forEach(n => console.log(n));
+// numbers.forEach(n => console.log(n));
+
+const fruits = ["Apple", "Banana", "Mango"];
+const list  = document.getElementById("list");
+const listItems = fruits.map((f) => {
+    const li = document.createElement("li");
+    li.textContent = f;
+    list.appendChild(li);
+});
