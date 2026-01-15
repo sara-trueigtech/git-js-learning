d706a23ab4c93b59468b6cb395a669bea664c041
sara-trueigtech <sakhan@trueigtech.com>
2026-01-15 15:26:39 +0530

    feat : Object with Methods (Functions inside objects)

--- objects.js
+++ objects.js
@@ -1,23 +1,32 @@
-let user = {
-  name: "John",
-  age: 30,
-  isAdmin: true,
-}; //object literal
+// let user = {
+//   name: "John",
+//   age: 30,
+//   isAdmin: true,
+// }; //object literal
 
-const admin = new Object(); //object constructor
-admin.name = "Sara";
-admin.age = 21;
+// const admin = new Object(); //object constructor
+// admin.name = "Sara";
+// admin.age = 21;
 
+// // let admin = user;
+// // admin.name = "sARA";
+// document.getElementById("btn").addEventListener("click", () => {
+//   // alert( user.hello === undefined );
+//   // alert( "adfdcyhsvchjvge" in user );
+// //   for (let key in user) {
+// //     // keys
+// //     alert(key); // name, age, isAdmin
+//     // // values for the keys
+//     // alert(user[key]); // John, 30, true
+//     alert(user.name);
+// });
 
-// let admin = user;
-// admin.name = "sARA";
-document.getElementById("btn").addEventListener("click", () => {
-  // alert( user.hello === undefined );
-  // alert( "adfdcyhsvchjvge" in user );
-//   for (let key in user) {
-//     // keys
-//     alert(key); // name, age, isAdmin
-    // // values for the keys
-    // alert(user[key]); // John, 30, true
-    alert(user.name);
-});
+const user = {
+  name: "Sara",
+  greet: function () {
+    return "Hello " + this.name;
+  },
+};
+
+console.log(user.greet());
+// this refers to the object only in normal functions, not arrow functions.
