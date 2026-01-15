6ce42e08cc2020081273ad96de761409bde07a57
sara-trueigtech <sakhan@trueigtech.com>
2026-01-15 16:10:27 +0530

    feat : array basics

---
+++ arrays.js
@@ -0,0 +1,14 @@
+// const numbers = [1, 2, 3, 4];
+// const mixed = ["Sara", 21, true, { city: "Indore" }];
+// const num = new Array(1, 2, 3);
+
+// console.log(numbers[0]); 
+// console.log(numbers[2]); 
+
+const numbers = [1, 2, 3];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+numbers.forEach(n => console.log(n));
